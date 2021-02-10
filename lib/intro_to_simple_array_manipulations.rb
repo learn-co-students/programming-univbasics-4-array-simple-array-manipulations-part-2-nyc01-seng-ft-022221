46d9dab8afa37604fb62706edf0bc752a9f11c08
Henry Velasquez <hvelas93@gmail.com>
2021-02-10 14:21:03 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(x,y)
+  x.concat(y)
+end
+
+def using_insert(x,y)
+  x.insert(4,y)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(x)
+  x.flatten
+end
+
+def using_delete(x,y)
+  x.delete(y)
+end
+
+  def using_delete_at(x,y)
+    x.delete_at(y)
+  end
